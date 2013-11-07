bb03691419c80b0c421ba6a1dd13b65148c46357
Mayank Meghwanshi <mayankmeghwanshi@gmail.com>
2013-11-07 23:27:49 +0530

    added service for getting candidate data

--- election-stats/WebContent/js/controllers.js
+++ election-stats/WebContent/js/controllers.js
@@ -8,14 +8,15 @@ function MenuCntl($scope, menuService) {
 	$scope.menu = menuService.getMenu();
 }
 
-function DataCntl($scope, dataService){
+function DataCntl($scope, Candidates, Discussions, dataService){
+
 	$scope.data = dataService.getData();
 	$scope.candidates = [
 		{
 			name: "Atal Bihari",
 			state: "Uttarprdesh",
 			constituency: "Lucknow",
-			vote: 901002,
+			vote: "901002",
 			votePercent: "20%",
 			result: "Won",
 			image: "atalji.jpg"
@@ -24,7 +25,7 @@ function DataCntl($scope, dataService){
 			name: "Atal Bihari2",
 			state: "Uttarprdesh",
 			constituency: "Lucknow",
-			vote: 901002,
+			vote: "901002",
 			votePercent: "20%",
 			result: "Won",
 			image: "atalji.jpg"
@@ -33,7 +34,16 @@ function DataCntl($scope, dataService){
 			name: "Atal Bihari3",
 			state: "Uttarprdesh",
 			constituency: "Lucknow",
-			vote: 901002,
+			vote: "901002",
+			votePercent: "20%",
+			result: "Won",
+			image: "atalji.jpg"
+		},
+		{
+			name: "Atal Bihari3",
+			state: "Uttarprdesh",
+			constituency: "Lucknow",
+			vote: "901002",
 			votePercent: "20%",
 			result: "Won",
 			image: "atalji.jpg"
@@ -43,34 +53,118 @@ function DataCntl($scope, dataService){
 		{
 			content: "Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.",
 			user: "Someone",
-			comments: 2,
-		},
-		{
-			content: "com1 Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.",
-			user: "Someone",
-			comments: 0,
-		},
-		{
-			content: "com2 Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.",
-			user: "Someone",
-			comments: 0,
+			comments: [
+				{
+					content: "com1 Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.",
+					user: "Someone",
+					comments: 0,
+				},
+				{
+					content: "com2 Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.",
+					user: "Someone",
+					comments: 0,
+				}
+			]
 		},
 		{
 			content: "dis2 Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.",
 			user: "Someone",
-			comments: 1,
-		},
-		{
-			content: "com3 Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.",
-			user: "Someone",
-			comments: 1,
-		},
-		{
-			content: "com31 sBacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.",
-			user: "Someone",
-			comments: 0,
+			comments: [
+				{
+					content: "com3 Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.",
+					user: "Someone",
+					comments: [
+						{
+							content: "com31 sBacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.",
+							user: "Someone",
+							comments: [
+								{
+									content: "com3 Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.",
+									user: "Someone",
+									comments: [
+										{
+											content: "com31 sBacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong.",
+											user: "Someone",
+											comments: 0,
+										}
+									]
+								}
+							]
+						}
+					]
+				}
+			]
 		}
 	];
+	if($scope.data.get){
+		var candidatePromise = Candidates.get({
+			election: $scope.data.election,
+			state: $scope.data.state,
+			constituency: $scope.data.constituency,
+			party: $scope.data.party,
+			personname: $scope.data.person.name,
+			persondob: $scope.data.person.dob,
+		});
+		candidatePromise.then(updateCandidates);
+	}
+	var discussionPromise = Discussions.get({
+		election: $scope.data.election,
+		state: $scope.data.state,
+		constituency: $scope.data.constituency,
+		party: $scope.data.party,
+		personname: $scope.data.person.name,
+		persondob: $scope.data.person.dob,
+	}, 0);
+	discussionPromise.then(updateDiscussion);
+
+	$scope.filter = function(){
+		$scope.data.filters.forEach(function(filter){
+			if(filter.type == "election")
+				$scope.data.election = filter.value;
+			if(filter.type == "state")
+				$scope.data.state = filter.value;
+			if(filter.type == "party")
+				$scope.data.party = filter.value;
+			if(filter.type == "constituency")
+				$scope.data.constituency = filter.value;
+			if(filter.type == "person")
+				$scope.data.person = filter.value;
+		});
+		if($scope.data.get){
+			var candidatePromise = Candidates.get({
+				election: $scope.data.election,
+				state: $scope.data.state,
+				constituency: $scope.data.constituency,
+				party: $scope.data.party,
+				personname: $scope.data.person.name,
+				persondob: $scope.data.person.dob,
+			}, 0);
+			candidatePromise.then(updateCandidates);
+		}
+		var discussionPromise = Discussions.get({
+			election: $scope.data.election,
+			state: $scope.data.state,
+			constituency: $scope.data.constituency,
+			party: $scope.data.party,
+			personname: $scope.data.person.name,
+			persondob: $scope.data.person.dob,
+		});
+		discussionPromise.then(updateDiscussion);
+	}
+	var updateCandidates = function(candidates){
+		console.log("ADF");
+		while ($scope.candidates.pop());
+		data.candidates.forEach(function(candidate) {
+			$scope.candidates.push(candidate);
+		});
+	}
+	var updateDiscussion = function(discussions){
+		while ($scope.discussions.pop());
+		data.discussions.forEach(function(discussion) {
+			$scope.discussions.push(discussion);
+		});
+	}
+
 }
 
 function HomeCntl($scope, menuService, dataService){
@@ -78,30 +172,41 @@ function HomeCntl($scope, menuService, dataService){
 		title: "Home"
 	});
 	dataService.updateParty("");
+	dataService.updateState("");
+	dataService.updateElection("");
+	dataService.updateConstituency("");
 	dataService.updateFilters([]);
+	dataService.updateGet(false);
 }
 
-function PartyCntl($scope, $route, Party, menuService, dataService){
+function PartyCntl($scope, $route, Party, List, menuService, dataService){
 	var partyPromise = Party.get($route.current.params.partyname);
 	partyPromise.then(function(p){
-		console.log(p);
 		$scope.party = p;
-		$scope.filters = [
-			{
+		$scope.filters = [];
+		var filterPromiseE = List.get("election", {});
+		filterPromiseE.then(function(elections){
+			$scope.filters.push({
 				type: "election",
-				values: [2009, 2004, 1999, 1998]
-			},
-			{
-				type: "state",
-				values: ["Andhra Pradesh", "Maharashtra", "Rajasthan", "NCT of Delhi"]
-			}
-		];
+				values: elections,
+				value: elections[0]
+			});
+			dataService.updateFilters($scope.filters);
+			var filterPromiseS = List.get("state", {});
+			filterPromiseS.then(function(states){
+				$scope.filters.push({
+					type: "state",
+					values: states,
+					value: states[0]
+				});
+				dataService.updateFilters($scope.filters);
+			});
+		});
+
 		menuService.update({
 			title: $scope.party.name
 		});
-		$scope.data = dataService.getData();
 		dataService.updateParty($scope.party.name);
-		dataService.updateFilters($scope.filters);
-		console.log($route);
+		dataService.updateGet(true);
 	});
 }
--- election-stats/WebContent/js/services.js
+++ election-stats/WebContent/js/services.js
@@ -16,16 +16,20 @@ angular.module('esi.services', []).service('menuService', function() {
 	}
 }).service('dataService', function() {
 	var data = {
+		get: false,
 		filters : [],
 		party : "",
 		state : "",
 		election : "",
-		constituency : ""
+		constituency : "",
+		person: {
+			name: "",
+			dob: "",
+		}
 	};
 	return {
 		updateFilters : function(filters) {
-			while (data.filters.pop())
-				;
+			while (data.filters.pop());
 			filters.forEach(function(filter) {
 				data.filters.push(filter);
 			});
@@ -42,8 +46,13 @@ angular.module('esi.services', []).service('menuService', function() {
 		updateConstituency : function(constituency) {
 			data.constituency = constituency;
 		},
+		updateGet: function(get){
+			data.get = get;
+		},
+		updatePerson: function(person){
+			data.person = person;
+		},
 		getData : function() {
-			console.log(data);
 			return data;
 		}
 	};
@@ -53,10 +62,42 @@ angular.module('esi.services', []).service('menuService', function() {
 	}
 
 	Party.get = function(name) {
-		console.log("he" + name);
 		return $http.get('/election-stats/api/party/' + name).then(function(response) {
 			return new Party(response.data);
 		});
 	};
 	return Party;
+}).factory('Candidates', function($http) {
+	var Candidates = function(data) {
+		angular.extend(this, data);
+	}
+
+	Candidates.get = function(filter) {
+		return $http.get('/election-stats/api/candidates', {params: filter}).then(function(response) {
+			return new Candidates(response.data);
+		});
+	};
+	return Candidates;
+}).factory('Discussions', function($http) {
+	var Discussions = function(data) {
+		angular.extend(this, data);
+	}
+
+	Discussions.get = function(filter, count) {
+		return $http.get('/election-stats/api/discussions/' + count, {params: filter}).then(function(response) {
+			return new Discussions(response.data);
+		});
+	};
+	return Discussions;
+}).factory('List', function($http) {
+	var List = function(data) {
+		angular.extend(this, data);
+	}
+
+	List.get = function(type, para) {
+		return $http.get('/election-stats/api/list/' + type, {params: para}).then(function(response) {
+			return new List(response.data);
+		});
+	};
+	return List;
 });
